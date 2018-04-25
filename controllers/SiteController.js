1e4dae7ce59e9a9943b4dbef317d2a33edbf40e3
nancy sin <nsin@sbcglobal.net>
2018-04-25 02:32:30 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict';
 
 const SiteController = {};
@@ -13,20 +13,6 @@ SiteController.About = function(req, resp) {
 
 SiteController.Contact = function(req, resp) {
   resp.render("site/contact")
-=======
-const SiteController = {};
-
-SiteController.Index = function(req, resp){
-  resp.render('site/index')
-}
-
-SiteController.About = function(req, resp){
-  resp.render('site/about')
-}
-
-SiteController.Contact = function(req, resp){
-  resp.render('site/contact')
->>>>>>> 96fb0453298fdb5ff7c4b80ab3210561c5b077ae
 }
 
 module.exports = SiteController
