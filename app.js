cf093100d7d6b77a2d9d1a37900bdd85bd7b8057
nancy sin <nsin@sbcglobal.net>
2018-04-25 02:34:08 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var app = express()
 const SiteController = require('./controllers/SiteController.js')
 
 
-app.get('/', Sitecontroller.Index)
+app.get("/", Sitecontroller.Index)
 
-app.get('/about', Sitecontroller.About)
+app.get("/about", Sitecontroller.About)
 
-app.get('/contact', Sitecontroller.Contact)
+app.get("/contact", Sitecontroller.Contact)
 
 
 module.exports = app
